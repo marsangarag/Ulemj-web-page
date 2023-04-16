5c73c52b97f95a2842351c0a6334114b05a7c4c4
amarsanaa0503 <amarsanaa0503@gmail.com>
2023-04-16 15:35:39 +0800

    fix products

--- src/components/home/product.tsx
+++ src/components/home/product.tsx
@@ -1,3 +1,7 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+
 export default function Products() {
     const products = [
         {
@@ -5,11 +9,7 @@ export default function Products() {
             price: "242.900",
             link: "https://shop.ulemjgrease.com/products/9256/92988",
         },
-        {
-            name: "Structruriste",
-            price: "219.900",
-            link: "https://shop.ulemjgrease.com/products/9256/92988",
-        },
+
         {
             name: "Cyfolia",
             price: "189.900",
@@ -21,9 +21,19 @@ export default function Products() {
             link: "https://shop.ulemjgrease.com/products/9256/92988",
         },
         {
+            name: "Structruriste",
+            price: "219.900",
+            link: "https://shop.ulemjgrease.com/products/9256/92988",
+        },
+        {
             name: "Line repair Teraskin+HA",
             price: "50.900",
             link: "https://shop.ulemjgrease.com/products/9256/35743",
+        },
+        {
+            name: "Wish Absolute Confidence",
+            price: "219.900",
+            link: "https://shop.ulemjgrease.com/products/9256/35615",
         },
     ];
 
@@ -32,36 +42,45 @@ export default function Products() {
     };
 
     return (
-        <div className="overflow-x-scroll w-full flex gap-x-1 sm:pb-4">
-            {products?.map((product) => {
-                return (
-                    <div
-                        onContextMenu={(e) => e.preventDefault()}
-                        key={product.name}
-                        className="relative"
-                    >
-                        <img
-                            src={`/images/products/${product.name}.jpg`}
-                            alt={product.name}
-                            className="min-w-[250px] min-h-[360px] sm:min-w-[333px] sm:min-h-[458px] rounded-xl"
-                        />
-                        <div className="absolute text-smaller sm:text-xs md:text-sm lg:text-base xl:text-lg bottom-4 text-white w-full px-4 flex items-center justify-between">
-                            <div className="flex flex-col">
-                                <div>{product?.name}</div>
-                                <div className="font-medium">
-                                    {product?.price}₮
+        <div>
+            <Swiper
+                spaceBetween={4}
+                slidesPerView={4}
+                centeredSlides={false}
+                grabCursor={true}
+                resistanceRatio={0}
+            >
+                {products?.map((product) => (
+                    <SwiperSlide>
+                        <div
+                            onContextMenu={(e) => e.preventDefault()}
+                            key={product.name}
+                            className="relative"
+                        >
+                            <img
+                                src={`/images/products/${product.name}.jpg`}
+                                alt={product.name}
+                                // className="min-w-[250px] min-h-[360px] sm:min-w-[333px] sm:min-h-[458px] rounded-xl"
+                                className="rounded-xl  w-full h-full aspect-auto "
+                            />
+                            <div className="absolute text-smaller sm:text-xs md:text-sm lg:text-base xl:text-lg md:bottom-4 bottom-2 text-white w-full md:px-4 px-2 flex flex-col md:flex-row gap-y-1 md:items-center items-start justify-between">
+                                <div className="flex flex-col shadow-text md:shadow-none">
+                                    <div>{product?.name}</div>
+                                    <div className="font-medium">
+                                        {product?.price}₮
+                                    </div>
+                                </div>
+                                <div
+                                    onClick={() => onShopClick(product?.link)}
+                                    className="rounded-3xl cursor-pointer border border-white py-1 px-1  md:px-2.5"
+                                >
+                                    Shop
                                 </div>
                             </div>
-                            <div
-                                onClick={() => onShopClick(product?.link)}
-                                className="rounded-3xl cursor-pointer border border-white py-1 px-1  md:px-2.5"
-                            >
-                                Shop
-                            </div>
                         </div>
-                    </div>
-                );
-            })}
+                    </SwiperSlide>
+                ))}
+            </Swiper>
         </div>
     );
 }
