2123233f9761b8984eca676ef33dd528a5f010e4
amarsanaa0503 <amarsanaa0503@gmail.com>
2023-04-16 16:31:39 +0800

    fix about page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,8 +1,8 @@
 export default function About() {
     return (
-        <div className="border-2 border-black/5">
+        <div className="border-2 border-black/5 rounded-xl container mx-auto">
             <div className="px-2 md:px-24 py-8 text-justify leading-5">
-                <h1 className="font-bold mb-8">
+                <h1 className="font-bold mb-8 text-center text-2xl">
                     SERVICE IS OUR BUSINESS PERFECTION IS OUR GOAL SATISFACTION
                     IS OUR GUARANTEE
                 </h1>
@@ -15,7 +15,7 @@ export default function About() {
                     (especially France, USA, and Germany) trends, and have been
                     adding the best technology and services to our customers.
                 </p>
-                <div className="mb-8 grid sm:grid-cols-3 gap-1">
+                <div className="mb-8 grid sm:grid-cols-3 gap-1 sm:gap-10">
                     <div className="border border-main rounded-2xl py-8 px-5 hover:bg-main ease-in duration-100 hover:text-white hover:scale-110 text-sm">
                         <h2 className="font-bold">MISSION</h2>
                         <p>
@@ -47,32 +47,32 @@ export default function About() {
                     equipments.
                 </p>
             </div>
-            <div className="grid sm:grid-cols-3 px-24 py-8 bg-black/5 gap-1">
+            <div className="grid grid-cols-3 px-4 sm:px-24 py-4 gap-1 sm:gap-10">
                 <div>
                     <img
                         src="/images/LPG.jpg"
-                        className="rounded-xl hover:scale-105 ease-in duration-100 hover:shadow-xl"
+                        className="rounded-xl hover:scale-105 ease-in duration-100 shadow-xl hover:shadow-xl"
                     />
                 </div>
                 <div>
                     <img
                         src="/images/M22.jpg"
-                        className="rounded-xl hover:scale-105 ease-in duration-100 hover:shadow-xl"
+                        className="rounded-xl hover:scale-105 ease-in duration-100 shadow-xl hover:shadow-xl"
                     />
                 </div>
                 <div>
                     <img
                         src="/images/Glowsolution.jpg"
-                        className="rounded-xl hover:scale-105 ease-in duration-100 hover:shadow-xl"
+                        className="rounded-xl hover:scale-105 ease-in duration-100 shadow-xl hover:shadow-xl"
                     />
                 </div>
             </div>
-            <div className="mt-8">
+            <div className="mt-8 px-4">
                 <h2 className="text-center font-bold">
                     Sustainability – Customer Service – Quality -
                     Personalization - Innovation - Professionalism and Teamwork.
                 </h2>
-                <div className="grid sm:grid-cols-4 px-24 items-center place-items-center">
+                <div className="grid grid-cols-4 sm:px-24 items-center place-items-center">
                     <img
                         src="/images/120staff.png"
                         alt=""
